19f33dbb946c7a982b110159e373a72925601c91
16thpolicy <33578435+16thpolicy@users.noreply.github.com>
2019-11-02 17:42:48 -0400

    Create mongo_client.py

---
+++ mongo_client.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+import datetime
+import pprint
+
+client = MongoClient()
+
+
+db = client.test_database
+
+collection = db.test_collection
+
+
+post = {"author": "Mike",
+         "text": "My first blog post!",
+         "tags": ["mongodb", "python", "pymongo"],
+         "date": datetime.datetime.utcnow()}
+
+posts = db.posts
+post_id = posts.insert_one(post).inserted_id
+print(post_id)
+
+print(db.list_collection_names())
+
+pprint.pprint(posts.find_one())
